f4ae8bbbf73414379817700bf914208c93bc1da7
Dhay <Diego50094@gmail.com>
2025-07-29 22:53:08 -0300

    move example chat bots to examples folder

--- examples/tool_chat.py
+++ examples/tool_chat.py
@@ -2,7 +2,26 @@ import google.generativeai as genai
 from google.generativeai import types
 import os
 from dotenv import load_dotenv
-from rpg_tools.agentic_tools.dice import JogarD20, D20_tool
+import random
+
+def JogarD20():
+    return random.randint(1,20)
+
+D20_tool = {
+    "name": "JogarD20",
+    # OBSERVAÇÃO: a escolha da decrição impacta significativamente no comportamento do modelo
+    "description": "Role um D20, gerando um número uniformemente aleatório entre 1 e 20. Chame quando for escolher um número aleatório após uma ação ou for rolar um D20.",
+    "parameters":{
+        "type": "object",
+        },
+}
+
+
+D20_tool_explanation = """Você tem acesso a um dado D20, pela função JogarD20
+Isso deve ser usado ao gerar um número aleatório, durante a seção de RPG com uma ação correspondente do usuário \
+ou então quando for pedido explicitamente.
+Não diga que rolou um D20 sem utilizar a ferramenta.
+"""
 
 tools = types.Tool(function_declarations=[D20_tool])
 
@@ -15,8 +34,8 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # Escolha o modelo
 model = genai.GenerativeModel('gemini-2.0-flash-lite', tools=[tools])
 
-chat = """Você ẽ um chat bot, que terã uma conversa com um usuãrio
-Você tem acesso a um dado D20, que deve ser usado para gerar números
+chat = f"""Você ẽ um chat bot, que terã uma conversa com um usuãrio
+{D20_tool_explanation}
 Agora iniciam as mensagens:
 """
 
